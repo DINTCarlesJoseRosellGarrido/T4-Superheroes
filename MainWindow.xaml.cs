e9f20d79ba3c38209085900170729f08653390a0
alumno <alumno@A302PC22>
2022-11-03 12:36:52 +0100

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using T4_Superheroes.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T4_Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Superheroe> super = Superheroe.GetSamples();
+        int index = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            DatosSuper.DataContext = super[index];
+        }
+
+        private void buttonPrevious_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(index == 0)
+            {
+                index = 2;
+            }
+            else
+            {
+                index--;
+            }
+
+            DatosSuper.DataContext = super[index];
+            numberChange.Text = (index+1).ToString();
+        }
+
+        private void buttonNext_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (index == 2)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+
+            DatosSuper.DataContext = super[index];
+            numberChange.Text = (index + 1).ToString();
+        }
+    }
+}
---
+++ Modelos/SuperHeroe.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_Superheroes.Modelos
+{
+    class Superheroe : INotifyPropertyChanged
+    {
+        private string nombre;
+        private string imagen;
+        private bool vengador;
+        private bool xmen;
+        private bool heroe;
+
+        public string Nombre 
+        { 
+            get { return nombre; } 
+            set {
+                nombre = value;
+                NotifyPropertyChanged("Nombre");
+            } 
+        }
+
+        public string Imagen
+        {
+            get { return imagen; }
+            set {
+                imagen = value;
+                NotifyPropertyChanged("Imagen");
+            }
+        }
+
+        public bool Vengador
+        {
+            get { return vengador; }
+            set {
+                vengador = value;
+                NotifyPropertyChanged("Vengador");
+            }
+        }
+
+        public bool Xmen
+        {
+            get { return xmen; }
+            set {
+                xmen = value;
+                NotifyPropertyChanged("Xmen");
+            }
+        }
+
+        public bool Heroe
+        {
+            get { return heroe; }
+            set {
+                heroe = value;
+                NotifyPropertyChanged("Heroe");
+            }
+        }
+
+        public Superheroe()
+        {
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+        }
+
+        public static List<Superheroe> GetSamples()
+        {
+            List<Superheroe> ejemplos = new List<Superheroe>();
+
+            Superheroe ironman = new Superheroe("Ironman", @"https://dossierinteractivo.com/wp-content/uploads/2021/01/Iron-Man.png", true, false, true);
+            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false);
+            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true);
+
+            ejemplos.Add(ironman);
+            ejemplos.Add(kingpin);
+            ejemplos.Add(spiderman);
+
+            return ejemplos;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
